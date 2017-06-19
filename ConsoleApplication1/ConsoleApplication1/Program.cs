73c72131e41eac0cbe9b38c535681c9b4e7f0186
JaVillarroell <javivillarroell@gmail.com>
2017-06-19 08:38:54 -0400

    Add files via upload

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using Newtonsoft.Json;
+
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+                                       
+
+           
+       var json = new WebClient().DownloadString("http://192.168.1.37/slimapp/public/api/customer/10");
+              List<Usuario> m = JsonConvert.DeserializeObject<List<Usuario>>(json);
+                Console.WriteLine(m.ElementAt(0).nit);
+                        
+                   
+
+        }
+    }
+}
---
+++ ConsoleApplication1/ConsoleApplication1/Usuario.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    public class Usuario
+    {
+        public int nit { get; set; }
+        public string direccion { get; set; }
+                public int telefono { get; set; }
+        
+    }
+    }
+    
+
